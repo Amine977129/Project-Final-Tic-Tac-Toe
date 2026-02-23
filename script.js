733733a002b1d3b2465dc1c7252524a52a1b6fc9
Amine977129 <mohammed.ali@estudiant.fjaverianas.com>
2026-02-23 02:28:07 +0100

    Entrega 3: HTML, CSS y JS con detección de ganador

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-let title=document.querySelector('#status')
-let turn='x';
+let title = document.querySelector('#status')
+let turn = 'x';
+let squares = [];
 
+function end(num1, num2, num3) {
 
+    title.innerHTML = `The winner is ${squares[num1]}`;
 
-function game (id) {
+    document.getElementById('item' + num1).style.background = '#00ff00';
+    document.getElementById('item' + num2).style.background = '#00ff00';
+    document.getElementById('item' + num3).style.background = '#00ff00';
 
-    let element= document.getElementById(id)
-    if (turn==='x' && element.innerHTML=='') {
-element.innerHTML='X';
-turn='o'
-title.innerHTML='Torn jugador O'
-        }
- else if (turn==='o' && element.innerHTML=='') {
-    element.innerHTML='O'
-    turn='x';
-    title.innerHTML='Torn jugador X'
+    setTimeout(function () {
+        location.reload();
+    }, 3000);
+}
 
- }
+function winner() {
 
+    for (let i = 1; i <= 9; i++) {
+        squares[i] = document.getElementById('item' + i).innerHTML;
+    }
+
+    if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != '') {
+        end(1, 2, 3);
+    }
+    else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != '') {
+        end(4, 5, 6);
+    }
+    else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != '') {
+        end(7, 8, 9);
+    }
+    else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != '') {
+        end(1, 4, 7);
+    }
+    else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != '') {
+        end(2, 5, 8);
+    }
+    else if (squares[3] == squares[6] && squares[6] == squares[9] && squares[3] != '') {
+        end(3, 6, 9);
+    }
+    else if (squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != '') {
+        end(1, 5, 9);
+    }
+    else if (squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != '') {
+        end(3, 5, 7);
+    }
+}
+
+function game(id) {
+
+    let element = document.getElementById(id)
+
+    if (turn === 'x' && element.innerHTML == '') {
+        element.innerHTML = 'X';
+        turn = 'o';
+        title.innerHTML = 'Turno jugador O';
+    }
+    else if (turn === 'o' && element.innerHTML == '') {
+        element.innerHTML = 'O';
+        turn = 'x';
+        title.innerHTML = 'Turno jugador X';
+    }
+
+    winner();
 }
